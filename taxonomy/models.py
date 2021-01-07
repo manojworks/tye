69aa598d2079a873edbdd085655c9645ab9503af
Manoj <manojworks@gmail.com>
2021-01-06 18:42:05 -0600

    Cleaned up loading taxonomy data

--- taxonomy/models.py
+++ taxonomy/models.py
@@ -1,13 +1,7 @@
-import pkgutil
-
 from django.conf import settings
 from django.db import models
 
-import data
 from data import product_taxonomy
-
-
-# Create your models here.
 
 
 class Taxonomy(models.Model):
@@ -23,17 +17,10 @@ class Taxonomy(models.Model):
     def load_taxonomy_to_model(cls):
         taxonomy_data = settings.DATA_PATH + "product_taxonomy_google.csv"
         taxonomy_dict = product_taxonomy.read_product_taxonomy_data(taxonomy_data)
-
+        # key is taxonomy id, val[0] is parent id, val[1] is product description
         for key, val in taxonomy_dict.items():
-            print(key)
-            print(val[0])
-            print(val[1])
             parent_instance = None
             if val[0] is not None:
                 parent_instance = Taxonomy.objects.get(pk=val[0])
-            print(parent_instance)
             t = Taxonomy(taxonomy_id=key, parent_id=parent_instance, product_description=val[1])
-            # t = Taxonomy("3237", "1", "Live Animals")
             t.save()
-            print(t)
-
