49faf062fbe011da6153fd16c6987e5ae3ec19f7
Manoj <manojworks@gmail.com>
2021-01-23 19:08:54 -0600

    Cleaned up URLs supported in this version

--- taxonomy/urls.py
+++ taxonomy/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.list_all, name='taxonomy_list_all'),
+    path('descr/<str:product_description>/', views.list_by_descr, name='taxonomy_list_by_description'),
+    path('id/<int:product_id>/', views.list_by_id, name='taxonomy_list_by_id'),
+    path('parent-id/<int:product_id>/', views.list_by_parent_id, name='taxonomy_list_by_parent_id'),
 ]
