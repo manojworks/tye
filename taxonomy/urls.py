0fc10340e825dc01606f38df3114d462052f498f
Manoj <manojworks@gmail.com>
2021-03-19 20:14:54 -0600

    Support for details view

--- taxonomy/urls.py
+++ taxonomy/urls.py
@@ -6,6 +6,6 @@ app_name = 'taxonomy'
 urlpatterns = [
     path('', views.list_all, name='taxonomy_list_all'),
     path('descr/<str:product_description>/', views.list_by_descr, name='taxonomy_list_by_description'),
-    path('id/<int:product_id>/', views.list_by_id, name='taxonomy_list_by_id'),
-    path('parent-id/<int:product_id>/', views.list_by_parent_id, name='taxonomy_list_by_parent_id'),
+    path('id/<str:pk>/', views.ProductDetailsView.as_view(), name='taxonomy_list_by_id'),
+    path('parent-id/<str:product_id>/', views.list_by_parent_id, name='taxonomy_list_by_parent_id'),
 ]
--- taxonomy/views.py
+++ taxonomy/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404, Http404
-from django.http import HttpResponse, JsonResponse
-from django.core import serializers
+from django.views import generic
 import json
 
 from .models import Taxonomy
@@ -12,7 +11,7 @@ def list_all(request):
     tmp_json = json.dumps(dict(list(list_of_taxonomies)))
     context = {
         'dict_of_taxonomies': tmp_json,
-        'page_tile': f'List of all Products'
+        'page_title': f'List of all Products'
     }
     return render(request, 'taxonomy/index.html', context)
 
@@ -21,6 +20,7 @@ def list_by_descr(request, product_description):
     product_details = Taxonomy.objects.values_list('taxonomy_id', 'product_description').\
             filter(product_description__icontains=product_description).order_by('product_description')
     if len(list(product_details)) == 0:
+        # TODO Instead of silly 404 show a better message on same page
         raise Http404(f"No Product Taxonomies with this description {product_description}")
 
     tmp_json = json.dumps(dict(list(product_details)))
@@ -28,10 +28,15 @@ def list_by_descr(request, product_description):
                                                    'page_title': f'List of all Products with description: {product_description}'})
 
 
-def list_by_id(request, product_id):
-    product_details = get_object_or_404(Taxonomy, pk=product_id)
-    return render(request, 'taxonomy/product-details.html', {'product_details': product_details,
-                                                             'page_title': f'Product Details for ID: {product_id}'})
+class ProductDetailsView(generic.DetailView):
+    model = Taxonomy
+    template_name = 'taxonomy/product-details.html'
+    context_object_name = 'product_details'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetailsView, self).get_context_data(**kwargs)
+        context['page_title'] = f'Product Details for ID:' + self.kwargs['pk']
+        return context
 
 
 def list_by_parent_id(request, product_id):
@@ -39,6 +44,7 @@ def list_by_parent_id(request, product_id):
         filter(parent_id=product_id).order_by('taxonomy_id')
 
     if len(list(product_details)) == 0:
+        # TODO Show a better page when not found
         raise Http404(f"No Product Taxonomies with this Parent ID: {product_id}")
 
     tmp_json = json.dumps(dict(list(product_details)))
